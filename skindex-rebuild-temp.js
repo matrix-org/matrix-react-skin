2fb058d66c9f14ed854e6c8f6b308a6c29d224f7
David Baker <dave@matrix.org>
2015-09-11 16:06:27 +0100

    Made this a proper thing now

--- skindex-rebuild-temp.js
+++
@@ -1,3 +0,0 @@
-# Note of the bash I've used to autogenerate the monotonous part of the skindex file. Maybe this could become a proper tool?
-
-find . | grep -e '.js$' | sed -e "s#\.\/\(.*\)\.js#ComponentBroker.set('\1\', require('./views/\1'));#"
