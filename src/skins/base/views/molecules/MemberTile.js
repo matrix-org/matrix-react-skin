106e33beea706555b3734e03a24e925be786fad3
David Baker <dave@matrix.org>
2015-09-17 12:10:52 +0100

    Add a membertile skin impl

--- src/skins/base/views/molecules/MemberTile.js
+++ src/skins/base/views/molecules/MemberTile.js
@@ -18,15 +18,69 @@ limitations under the License.
 
 var React = require('react');
 
-var MemberTileController = require('matrix-react-sdk/lib/controllers/molecules/MemberTile');
+var sdk = require('matrix-react-sdk');
+var MatrixClientPeg = require("matrix-react-sdk/lib/MatrixClientPeg");
+var Modal = require("matrix-react-sdk/lib/Modal");
+var MemberTileController = require("matrix-react-sdk/lib/controllers/molecules/MemberTile");
+var MemberInfo = sdk.getComponent('molecules.MemberInfo');
+var ErrorDialog = sdk.getComponent("organisms.ErrorDialog");
+var MemberAvatar = sdk.getComponent('atoms.MemberAvatar');
+
+// The Lato WOFF doesn't include sensible combining diacritics, so Chrome chokes on rendering them.
+// Revert to Arial when this happens, which on OSX works at least.
+var zalgo = /[\u0300-\u036f\u1ab0-\u1aff\u1dc0-\u1dff\u20d0-\u20ff\ufe20-\ufe2f]/;
 
 module.exports = React.createClass({
     displayName: 'MemberTile',
     mixins: [MemberTileController],
+
     render: function() {
+        var isMyUser = MatrixClientPeg.get().credentials.userId == this.props.member.userId;
+
+        var presenceClass = "mx_MemberTile_offline";
+        var mainClassName = "mx_MemberTile ";
+        if (this.props.member.user) {
+            if (this.props.member.user.presence === "online") {
+                presenceClass = "mx_MemberTile_online";
+            }
+            else if (this.props.member.user.presence === "unavailable") {
+                presenceClass = "mx_MemberTile_unavailable";
+            }
+        }
+        mainClassName += presenceClass;
+
+        var name = this.props.member.name;
+        if (isMyUser) name += " (me)";
+        var leave = isMyUser ? <span className="mx_MemberTile_leave" onClick={this.onLeaveClick}>X</span> : null;
+
+        var nameClass = this.state.hover ? "mx_MemberTile_nameSpan" : "mx_MemberTile_name";
+        if (zalgo.test(name)) {
+            nameClass += " mx_MemberTile_zalgo";
+        }
+
+        var nameEl;
+        if (this.state.hover) {
+            nameEl =
+                <div className="mx_MemberTile_nameWrapper">
+                    <MemberInfo member={this.props.member} />
+                    <span className={nameClass}>{name}</span>
+                    {leave}
+                </div>
+        }
+        else {
+            nameEl =
+                <div className={nameClass}>
+                    {name}
+                    {leave}
+                </div>
+        }
+
         return (
-            <div className="mx_MemberTile">
-                <div className="mx_MemberTile_name">{this.props.member.name}</div>
+            <div className={mainClassName} onMouseEnter={ this.mouseEnter } onMouseLeave={ this.mouseLeave }>
+                <div className="mx_MemberTile_avatar">
+                    <MemberAvatar member={this.props.member} />
+                </div>
+                { nameEl }
             </div>
         );
     }
