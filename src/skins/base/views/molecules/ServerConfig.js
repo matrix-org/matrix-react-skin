64712e3d076ad6b939c36d471b0b6e24f8be7421
David Baker <dave@matrix.org>
2015-09-18 13:34:12 +0100

    Port serverconfig

--- src/skins/base/views/molecules/ServerConfig.js
+++ src/skins/base/views/molecules/ServerConfig.js
@@ -17,26 +17,33 @@ limitations under the License.
 'use strict';
 
 var React = require('react');
+var Modal = require('matrix-react-sdk/lib/Modal');
+var sdk = require('matrix-react-sdk');
 
-var ServerConfigController = require('matrix-react-sdk/lib/controllers/molecules/ServerConfig');
+var ServerConfigController = require("matrix-react-sdk/lib/controllers/molecules/ServerConfig");
 
 module.exports = React.createClass({
     displayName: 'ServerConfig',
     mixins: [ServerConfigController],
 
+    showHelpPopup: function() {
+        var ErrorDialog = sdk.getComponent('organisms.ErrorDialog');
+        Modal.createDialog(ErrorDialog, {
+          title: 'Custom Server Options',
+          description: "You can use the custom server options to log into other Matrix servers by specifying a different Home server URL. This allows you to use Vector with an existing Matrix account on a different Home server. You can also set a cutom Identity server but this will affect people ability to find you if you use a server in a group other than tha main Matrix.org group.",
+          button: "Dismiss",
+          focus: true
+        });
+    },
+
     render: function() {
         return (
-            <div className="HomeServerTextBox">
-                <table className="serverConfig">
-                <tr>
-                <td>Home Server URL</td>
-                <td><input type="text" value={this.state.hs_url} onChange={this.hsChanged} /></td>
-                </tr>
-                <tr>
-                <td>Identity Server URL</td>
-                <td><input type="text" value={this.state.is_url} onChange={this.isChanged} /></td>
-                </tr>
-                </table>
+            <div className="mx_ServerConfig">
+                <label className="mx_Login_label mx_ServerConfig_hslabel" htmlFor="hsurl">Home server URL</label>
+                <input className="mx_Login_field" id="hsurl" type="text" value={this.state.hs_url} onChange={this.hsChanged} />
+                <label className="mx_Login_label mx_ServerConfig_islabel" htmlFor="isurl">Identity server URL</label>
+                <input className="mx_Login_field" type="text" value={this.state.is_url} onChange={this.isChanged} />
+                <a className="mx_ServerConfig_help" href="#" onClick={this.showHelpPopup}>What does this mean?</a>
             </div>
         );
     }
